73f0c2c8d07b54a352786e7ef0e4a867a813599a
AdamBenDev <adamaya9732@gmail.com>
2023-03-07 17:08:20 +0100

    Exercice completed

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,28 @@
 
-let nome = prompt("Qual è il tuo nome?");
-console.log(nome);
+//! NOME
+let name = prompt("Qual è il tuo nome?");
+console.log(name);
+
+
+
+//!COGNOME
+let surname = prompt("Qual è il tuo cognome?");
+console.log(surname);
+
+
+
+
+//!COLORE PREFERITO
+let color = prompt("Qual è il tuo colore preferito?");
+console.log(color);
+
+
+
+//!RISULTATO 
+
+let nome = "Adam";
+let cognome = "Ben";
+let colorePreferito = "rosso";
+
+let risultato = nome + cognome + colorePreferito + "23";
+document.write(risultato)
